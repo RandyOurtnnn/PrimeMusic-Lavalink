b38f858378ac522019e9b65d5b3d15b302f7d540
RandyOurtnnn <128302639+RandyOurtnnn@users.noreply.github.com>
2025-03-16 00:04:57 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
+  mongodbUri : "mongodb+srv://yousefmasoud221:<lOYNJDouNxFzYISI>@cluster0.t6yb6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
   spotifyClientId : "",
   spotifyClientSecret : "",
   setupFilePath: './commands/setup.json',
